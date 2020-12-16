6c2e7cc7b525bd4e1d6eeb4492e4d67fd34babd1
SaraMonts <61522863+SaraMonts@users.noreply.github.com>
2020-12-16 13:07:35 +0100

    Es modifica la funció as.data.frame.pedigree per a obtenir un data frame que contingui totes les columnes d'affected

--- R/as.data.frame.pedigree.R
+++ R/as.data.frame.pedigree.R
@@ -7,10 +7,18 @@ as.data.frame.pedigree <- function(x, ...) {
   momid[x$mindex>0] <- x$id[x$mindex]
   df <- data.frame(id=x$id, dadid=dadid, momid=momid, sex=x$sex)
   
-  if(!is.null(x$affected))
-    df$affected = x$affected
+  if(!is.null(x$affected)) {
+    for (i in 1:ncol(x$affected)) {
+      aff <- x$affected[,i]
+      if (i == 1) df$affected1 <- aff
+      else if (i == 2) df$affected2 <- aff
+      else if (i == 3) df$affected3 <- aff
+      else if (i == 4) df$affected4 <- aff
+    }
+  }
   
   if(!is.null(x$status))
     df$status = x$status
   return(df)
 }
+
